ded5c958be2a2c82d6deae12a4cbb20415ff3f64
puffyboa <julian@sankergroup.org>
2019-11-27 01:26:32 -0600

    testing another possible fix for bad request uploading error

--- app/main/routes.py
+++ app/main/routes.py
@@ -103,13 +103,11 @@ def make_post(chat_name):
         new_post = Post(title=form.title.data, body=form.body.data, author=current_user)
         new_post.chat = Chat.query.filter_by(name=chat_name).first()
 
-        if request.files.get('image'):
-            file = request.files['image']
-            if file:
-                filename = images.save(file)
-                url = images.url(filename)
-                image = Image(filename=filename, url=url)
-                new_post.attachment = image
+        if 'image' in request.files and request.files['image']:
+            filename = images.save(form.image.data)
+            url = images.url(filename)
+            image = Image(filename=filename, url=url)
+            new_post.attachment = image
 
         db.session.add(new_post)
         db.session.commit()
