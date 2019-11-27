e02c6cdce3168c8742ecebb416303094ec57ff05
puffyboa <julian@sankergroup.org>
2019-11-27 11:21:54 -0600

    added error handling for file too large error

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import db
 from app.errors import bp
+from werkzeug.exceptions import RequestEntityTooLarge
 
 
 @bp.app_errorhandler(404)
@@ -12,3 +13,9 @@ def not_found_error(error):
 def internal_error(error):
     db.session.rollback()
     return render_template('errors/500.html'), 500
+
+
+@bp.errorhandler(413)
+@bp.errorhandler(RequestEntityTooLarge)
+def file_too_large_error(error):
+    return render_template('errors/413.html'), 413
