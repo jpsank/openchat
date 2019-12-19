d314dbacde90931d32f239a56452bcdc24b5a5c4
puffyboa <julian@sankergroup.org>
2019-12-19 17:27:05 -0600

    add censorship of explicit language

--- app/util/filters.py
+++ app/util/filters.py
@@ -1,10 +1,25 @@
 from flask import escape
 from app import app
+import re
 
 
 # @app.template_filter()
 def nl2br(text):
     return text.replace('\n', '<br>')
+
+
+@app.template_filter()
+def censor(text):
+    bad_words = {
+        'dick': 1,
+        'fuck': 1,
+        'cock': 1
+    }
+    for bad_word, i in bad_words.items():
+        for m in re.finditer(bad_word, text, re.IGNORECASE):
+            ch_idx = m.start()+i
+            text = text[:ch_idx] + '*' + text[ch_idx+1:]
+    return text
 
 
 # @app.context_processor
